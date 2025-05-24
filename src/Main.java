56500df5daa726c93e8cd36820f106364a118b13
kapkoti-dotcom <kapkotihimanshi99@gmail.com>
2025-05-24 17:10:57 +0530

    "Day 10:" medium level(bubble sort)"

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,35 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {5, 1, 4, 2, 8};
-        bubbleSort(arr);
-        System.out.println("Sorted array:");
-        for (int num : arr) {
-            System.out.println(num + " ");
-        }
+        int[] arr = {1,2,3,4,5};
+        int totalSwaps = Main(arr);
+        System.out.println("total number of swaps:" + totalSwaps);
     }
-    static void bubbleSort(int[]arr){
-        int n= arr.length;
-        for(int i=0;i<n-1;i++){
-            boolean swapped=false;
-            for(int j=0;j<n-1-i;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+
+    public static int Main(int[] arr) {
+        int n = arr.length;
+        int swapCount = 0;
+        boolean swapped=false;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapCount++;
                     swapped=true;
                 }
             }
-            if(!swapped)break;
+            if(!swapped){
+                break;
+            }
         }
+        return swapCount;
     }
-
-
-
-
 }
+
+
+
+
+
