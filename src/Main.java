fb92ee454777c51de15e1dc58335fdfaa4a4e66f
kapkoti-dotcom <kapkotihimanshi99@gmail.com>
2025-05-26 18:26:13 +0530

    "Day 12:"selection sort(sort but count swap :"

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {9, 5, 1, 4, 3};
-        isMain(arr);
+        int[] arr = {9,  1, 4,5, 3};
+
+        int swaps=isMain(arr);
         System.out.println("sorted array");
         for (int num : arr) {
             System.out.println(num + "");
         }
+        System.out.println("no of swaps:"+swaps);
     }
 
-    static void isMain(int[] arr) {
+    static int isMain(int[] arr) {
         int n = arr.length;
+        int swapCount = 0;
 
         for (int i = 0; i < n - 1; i++) {
             int si = i;
@@ -20,10 +23,15 @@ public class Main {
                 }
             }
 
-
-            int temp = arr[si];
-            arr[si] = arr[i];
-            arr[i] = temp;
+            if (si != i) {
+                int temp = arr[si];
+                arr[si] = arr[i];
+                arr[i] = temp;
+                swapCount++;
+            }
         }
+
+
+        return swapCount;
     }
 }
