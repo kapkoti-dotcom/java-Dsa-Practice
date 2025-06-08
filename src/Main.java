a713401b89aeff681d5996296161bc368b269185
kapkoti-dotcom <kapkotihimanshi99@gmail.com>
2025-06-08 22:48:55 +0530

    "day 18"revise insertion sorting "

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public  static void main(String[]args){
-        int[]arr={64,25,12,22,11};
-        selectionSort(arr);
+        int[]arr={5,3,4,1,2};
+        insertionSort(arr);
         for(int num:arr){
             System.out.println(num+" ");
         }
@@ -9,20 +9,26 @@ public class Main {
 
 
     }
-    static void selectionSort(int[]arr){
+    static void insertionSort(int[]arr){
         for(int i=0;i<arr.length-1;i++){
-        int minIndex=i;
-        for(int j=i+1;j<arr.length;j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
+             for(int j=i+1;j>0;j--) {
+                 if (arr[j] < arr[j-1]) {
+                     swap(arr,j,j-1);
 
-            }
-
-        }
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
+            }else{
+                     break;
+                 }
 
         }
+
+
+        }
+    }
+    static void swap(int[]arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+
+
     }
 }
