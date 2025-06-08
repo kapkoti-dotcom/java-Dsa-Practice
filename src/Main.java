0e058943212c7f4acb4b4ba5832ca554dcb70c56
kapkoti-dotcom <kapkotihimanshi99@gmail.com>
2025-06-08 22:34:37 +0530

    "day 17"revise selection sorting "

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,28 @@
 public class Main {
     public  static void main(String[]args){
-        int[]arr={7,2,5,1,9};
-        bubbleSort(arr);
+        int[]arr={64,25,12,22,11};
+        selectionSort(arr);
         for(int num:arr){
             System.out.println(num+" ");
         }
 
 
-    }
-    static void bubbleSort(int[]arr){
-        for(int i=0;i< arr.length-1;i++) {
-            for (int j = 0; j < arr.length - 1-i; j++) {
-                if (arr[j] > arr[j + 1]) {
 
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j+ 1] = temp;
-                }
+    }
+    static void selectionSort(int[]arr){
+        for(int i=0;i<arr.length-1;i++){
+        int minIndex=i;
+        for(int j=i+1;j<arr.length;j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+
             }
+
+        }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+
         }
     }
 }
