02aa4e2d2907ecf056eef76cd71d4b949707ff8a
kapkoti-dotcom <kapkotihimanshi99@gmail.com>
2025-06-09 22:37:09 +0530

    "day 19":string questions"

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,9 @@
 public class Main {
-    public  static void main(String[]args){
-        int[]arr={5,3,4,1,2};
-        insertionSort(arr);
-        for(int num:arr){
-            System.out.println(num+" ");
-        }
-
-
-
-    }
-    static void insertionSort(int[]arr){
-        for(int i=0;i<arr.length-1;i++){
-             for(int j=i+1;j>0;j--) {
-                 if (arr[j] < arr[j-1]) {
-                     swap(arr,j,j-1);
-
-            }else{
-                     break;
-                 }
-
-        }
-
-
-        }
-    }
-    static void swap(int[]arr,int i,int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-
-
+    public static void main(String[] args) {
+       StringBuilder sb = new StringBuilder("Google");
+       sb.insert(0,"Hello ");
+       sb.delete(sb.length()-2,sb.length());
+       sb.reverse();
+        System.out.println(sb.toString());
     }
 }
