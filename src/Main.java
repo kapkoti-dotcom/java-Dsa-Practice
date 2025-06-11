edd257bf3518fecdcdb8f7fb8ec94c7c430fc6e6
kapkoti-dotcom <kapkotihimanshi99@gmail.com>
2025-06-11 22:35:48 +0530

    "day 20":recursion questions"

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,18 @@
 public class Main {
-    public static boolean isPalindrome(String str) {
-
-        if (str.length() == 0 || str.length() == 1) {
-            return true;
-        }
-
-        if (str.charAt(0) != str.charAt(str.length() - 1)) {
-            return false;
-        }
-
-        return isPalindrome(str.substring(1, str.length() - 1));
-    }
-
     public static void main(String[] args) {
-        String input = "madam";
-        if (isPalindrome(input)) {
-            System.out.println(input + " is a Palindrome.");
-        } else {
-            System.out.println(input + " is NOT a Palindrome.");
+        int num=5;
+        int result=factorial(num);
+        System.out.println("factorial of "+num+"is"+result);
+
+
+    }
+    static int factorial(int n){
+
+        if(n==0){
+            return 1;
+
+
         }
+        return n*factorial(n-1);
     }
 }
