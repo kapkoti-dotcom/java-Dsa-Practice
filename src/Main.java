bb188e4d13331de3636a3ce05838e0506c9c645b
kapkoti-dotcom <kapkotihimanshi99@gmail.com>
2025-06-24 15:46:37 +0530

    "day 20":Anagram logic"

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        int num=5;
-        int result=factorial(num);
-        System.out.println("factorial of "+num+"is"+result);
+import java.util.Arrays;
 
+class Main{
+    public static void main(String[] args) {
 
     }
-    static int factorial(int n){
-
-        if(n==0){
-            return 1;
-
-
+    static int Anagram(String S1,String S2){
+        if(S1.length()!=S2.length()){
+            return 0;
         }
-        return n*factorial(n-1);
+        char[]arr1=S1.toCharArray();
+        char[]arr2=S2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        for(int i=0;i<arr1.length;i++){
+            if(arr1[i]!=arr2[i]){
+                return 0;
+            }
+        }
+        return 1;
+
     }
 }
+
+
+
+
