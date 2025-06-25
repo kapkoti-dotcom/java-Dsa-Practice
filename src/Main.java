30d56ddaa45e89f1873e504d57f8820bf4b21c3c
kapkoti-dotcom <kapkotihimanshi99@gmail.com>
2025-06-25 22:50:21 +0530

    "day 20"java medium code"

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,17 @@
-import java.util.Arrays;
-
-class Main{
-    public static void main(String[] args) {
-
-    }
-    static int Anagram(String S1,String S2){
-        if(S1.length()!=S2.length()){
-            return 0;
-        }
-        char[]arr1=S1.toCharArray();
-        char[]arr2=S2.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        for(int i=0;i<arr1.length;i++){
-            if(arr1[i]!=arr2[i]){
-                return 0;
+class Main {
+    public static double posAverage(int[] arr) {
+        // Code here
+        int n=arr.length;
+        double ans=0.0;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=0){
+                count++;
+                ans+=arr[i];
             }
         }
-        return 1;
-
+        if(count<=0)return 0.0;
+        double avg=ans/count;
+        return avg;
     }
 }
-
-
-
-
