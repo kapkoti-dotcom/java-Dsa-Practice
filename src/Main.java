7b2ac2cb39ef42510efaba26cfb3f7c887838ca8
kapkoti-dotcom <kapkotihimanshi99@gmail.com>
2025-09-04 10:23:16 +0530

    day 30=binary search

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,35 @@
 class Main{
-    public static void main(String[] args) {
-        int arr[] = new int[10];
-        arr[0]=2;
-        arr[1]=4;
-        arr[2]=5;
-        arr[3]=7;
-        arr[4]=8;
-        arr[5]=78;
-        int n= 6;
-            int pos = 1;
-            int element = 3;
-            for(int i=n;i>pos;i--) {
-                arr[i ] = arr[i-1];
+    public int[] searchRange(int[] nums, int target) {
+        int res[]=new int[2];
+        int first=binarySearch(nums,target,true);
+        int last=binarySearch(nums,target,false);
+        res[0]=first;
+        res[1]=last;
+        return res;
 
-            }
-            arr[pos] = element;
-            n++;
-            for(int i=0;i<n;i++){
-                System.out.println(arr[i]+" ");
+
+    }
+    static int binarySearch(int arr[],int target,boolean isFirst){
+        int ans=-1;
+        int start=0;
+        int end= arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                ans= mid;
+                if(isFirst){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }else if(arr[mid]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
             }
 
         }
+        return ans;
 
     }
-
+}
