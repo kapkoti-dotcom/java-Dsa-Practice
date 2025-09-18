bd3b058973a8ed39e0371bfe959d9bc2196a701f
kapkoti-dotcom <kapkotihimanshi99@gmail.com>
2025-09-18 09:45:36 +0530

    day 33"binary search

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,39 @@
 import java.util.Arrays;
 
-class Main{
+class Main {
     public static void main(String[] args) {
-        int arr[] = {10, 30, 25, 45,35,555,};
-       int index= jumpsearch(arr,35);
+        int arr[] = {10, 30, 25, 45, 35, 555,};
+        int index=ternarySearch(arr,555);
         if(index==-1){
             System.out.println("element not found");
         }else{
-            System.out.println("element found at index:"+index);
+            System.out.println("found at element at index"+index);
         }
+
 
     }
-       /* findMaxMin(arr);
-    static void findMaxMin(int arr[]){
-        if(arr.length==0) {
-            System.out.println("Array is empty");
-            return;
 
-        }
-        int Max_val=arr[0];
-        int Min_val=arr[0];
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]>Max_val){
-                Max_val=arr[i];
-            }
-            if(arr[i]<Min_val){
-                Min_val=arr[i];
-            }
+    /* findMaxMin(arr);
+ static void findMaxMin(int arr[]){
+     if(arr.length==0) {
+         System.out.println("Array is empty");
+         return;
 
-        }
-        System.out.println("maximum element"+Max_val);
-        System.out.println("minimum element"+Min_val);
-    }
+     }
+     int Max_val=arr[0];
+     int Min_val=arr[0];
+     for(int i=1;i<arr.length;i++){
+         if(arr[i]>Max_val){
+             Max_val=arr[i];
+         }
+         if(arr[i]<Min_val){
+             Min_val=arr[i];
+         }
+
+     }
+     System.out.println("maximum element"+Max_val);
+     System.out.println("minimum element"+Min_val);
+ }
 }*/
 /*static void ReverseArray(int arr[]){
     int left=0;
@@ -65,7 +67,7 @@ class Main{
     System.out.println("largest element "+secondlargest);
 }
 }*/
-public static int jumpsearch(int[]arr,int target){
+/*public static int jumpsearch(int[]arr,int target){
     int n=arr.length;
     int bs=(int)Math.sqrt(n);
     int start=0;
@@ -83,5 +85,34 @@ public static int jumpsearch(int[]arr,int target){
         }
     }
     return res;
+    }
+}*/
+    public static int ternarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length -1;
+
+        int index = -1;
+        while (start <= end) {
+            int mid1 = start + (end - start) / 3;
+            int mid2 = end - (end - start) / 3;
+            if (target <= arr[mid1]) {
+                if (target == arr[mid1]) {
+                    index = mid1;
+                    break;
+                }
+                end = mid1 - 1;
+            } else if (target >= arr[mid2]) {
+                if (target == arr[mid2]) {
+                    index = mid2;
+                    break;
+
+                }
+                start = mid2 + 1;
+            } else {
+                start = mid1 + 1;
+                end = mid2 - 1;
+            }
+        }
+        return index;
     }
 }
